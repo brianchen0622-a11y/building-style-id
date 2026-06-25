ef45986511441869d8ced3d77558ca8920e004c6
brianchen0622-a11y <295761953+brianchen0622-a11y@users.noreply.github.com>
2026-06-25 19:47:37 +0800

    Activate new service worker immediately on update
    Without skipWaiting/clients.claim, a deployed update only took effect
    after the PWA/tab was fully closed and reopened twice.

--- sw.js
+++ sw.js
@@ -13,14 +13,16 @@ const SHELL = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
